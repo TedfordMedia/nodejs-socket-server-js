a0767a35c7f69ed3a1514a2ee09708b622541d99
TedfordMedia <ted@tedfordmedia.com>
2022-11-23 22:24:08 +0000

    init ok

--- src/app.js
+++ src/app.js
@@ -6,22 +6,9 @@ const server = http.createServer(app);
 const hostname = '0.0.0.0';
 const port = 80;
 
-
-
-// const server = http.createServer((req, res) => {
-
-//     res.statusCode = 200;
-
-//     res.setHeader('Content-Type', 'text/plain');
-
-//     res.end('Hello World v1');
-
-// });
-
 app.get('/', (req, res) => {
     res.send('<h1>Hello world app get </h1>');
 });
-
 
 server.listen(port, hostname, () => {
 
