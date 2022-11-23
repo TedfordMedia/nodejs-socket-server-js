b648ff078007bcb8514ae063ce4882ce16c33436
TedfordMedia <ted@tedfordmedia.com>
2022-11-23 19:57:44 +0000

    init

--- src/app.js
+++ src/app.js
@@ -1,32 +1,56 @@
+const express = require('express');
 const http = require('http');
 
-
-
+const app = express();
 const hostname = '0.0.0.0';
 
 const port = 80;
 
 
+// const server = http.createServer((req, res) => {
+//     res.statusCode = 200;
+//     res.setHeader('Content-Type', 'text/plain');
+//     res.end('Hello World v1');
+// });
 
-const server = http.createServer((req, res) => {
 
+// server.listen(port, hostname, () => {
+
+//     console.log(`Server running at http://${hostname}:${port}/`);
+
+// });
+
+
+app.get('/', (req, res) => {
     res.statusCode = 200;
-
     res.setHeader('Content-Type', 'text/plain');
+    res.send('<h1>Hello world</h1>');
+});
 
-    res.end('Hello World v1');
 
+server.listen(3000, () => {
+    console.log('listening on *:3000');
 });
 
 
 
-server.listen(port, hostname, () => {
 
-    console.log(`Server running at http://${hostname}:${port}/`);
+// const server = http.createServer(app);
 
-});
+// app.get('/', (req, res) => {
+//   res.send('<h1>Hello world</h1>');
+// });
 
-process.on('SIGINT', function() {
+// server.listen(3000, () => {
+//   console.log('listening on *:3000');
+// });
+
+
+
+
+
+
+process.on('SIGINT', function () {
 
     // sigint signal received, log a message to the console
 
