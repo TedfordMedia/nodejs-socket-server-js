0ba7bfffb321a62631383d970e5a215569a4f80d
TedfordMedia <ted@tedfordmedia.com>
2022-11-23 22:48:33 +0000

    init

--- src/app.js
+++ src/app.js
@@ -7,11 +7,12 @@ const hostname = '0.0.0.0';
 const port = 80;
 
 app.get('/', (req, res) => {
-    res.send('<h1>Hello world app get </h1>');
+  // res.send('<h1>Hello world app get </h1>');
+  res.sendFile(__dirname + '/html/index.html');
 });
 
 server.listen(port, hostname, () => {
 
-    console.log(`Server running at http://${hostname}:${port}/`);
+  console.log(`v1.0 Server running at http://${hostname}:${port}/`);
 
 });
