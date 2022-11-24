35b3c1ec7e034bafd15c960c3da3b90a6d983653
TedfordMedia <ted@tedfordmedia.com>
2022-11-24 00:28:25 +0000

    socketio setup

--- src/app.js
+++ src/app.js
@@ -2,19 +2,25 @@ const http = require('http');
 const express = require('express');
 const app = express();
 const server = http.createServer(app);
-
+const { Server } = require("socket.io");
 const hostname = '0.0.0.0';
 const port = 80;
+const io = new Server(server);
 
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/html/index.html');
 });
+
 app.get('/chat', (req, res) => {
   res.sendFile(__dirname + '/html/chat.html');
 });
 
+io.on('connection', (socket) => {
+  console.log('a user connected');
+});
+
 server.listen(port, hostname, () => {
 
-  console.log(`v1.23 Server running at http://${hostname}:${port}/`);
+  console.log(`v2 Server running at http://${hostname}:${port}/`);
 
 });
---
+++ src/systemdata.js
@@ -0,0 +1,3 @@
+///declare variable for version number and export it
+export const version = "1.1.1";
+
