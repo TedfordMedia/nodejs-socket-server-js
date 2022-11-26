7251ccd4d15f92c31dc66661b8fe00c8bf9d5e63
Ted - TedfordMedia <ted@tedfordmedia.com>
2022-11-26 20:14:23 +0000

    init

--- src/html/js/threeScene.js
+++ src/html/js/threeScene.js
@@ -1,1 +1,39 @@
-console.log('threescene.js loaded');
+console.log('v1.1 threescene.js loaded');
+const container = document.getElementById('container');
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera(
+    75,
+    window.innerWidth / window.innerHeight,
+    0.1,
+    1000
+);
+
+const renderer = new THREE.WebGLRenderer({ antialias: true });
+renderer.setPixelRatio(window.devicePixelRatio);
+renderer.setSize(window.innerWidth, window.innerHeight);
+renderer.outputEncoding = THREE.sRGBEncoding;
+renderer.shadowMap.enabled = true;
+container.appendChild(renderer.domElement);
+
+const geometry = new THREE.BoxGeometry();
+const material = new THREE.MeshStandardMaterial({ color: 0x00ff00 });
+const cube = new THREE.Mesh(geometry, material);
+scene.add(cube);
+
+const ambientLight = new THREE.AmbientLight(0xffffff, 0.5);
+
+camera.position.z = 5;
+
+function onWindowResize() {
+
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
+
+    renderer.setSize(window.innerWidth, window.innerHeight);
+
+}
+function animate() {
+    requestAnimationFrame(animate);
+    renderer.render(scene, camera);
+}
+animate();
