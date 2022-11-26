947ea7087a0d44e7fa4290f58bc7b0afa5106d0f
Ted - TedfordMedia <ted@tedfordmedia.com>
2022-11-26 21:20:03 +0000

    init

--- src/html/js/threeScene.js
+++ src/html/js/threeScene.js
@@ -1,14 +1,11 @@
-import * as THREE from 'https://cdn.skypack.dev/three@0.129.0/build/three.module.js';
-
 import threeTools from './threeTools.js';
 console.log('v1.1 threescene.js loaded');
 
 threeTools.init();
 threeTools.setBasicCamera();
-
-
 threeTools.setLighting();
 threeTools.makeCube();
+threeTools.setBackdrop();
 
 window.addEventListener( 'resize', threeTools.onWindowResize );
  
