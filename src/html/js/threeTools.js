beb072363e3a42947a44602c75070e01770cedb4
TedfordMedia <ted@tedfordmedia.com>
2022-11-26 21:29:40 +0000

    init

--- src/html/js/threeTools.js
+++ src/html/js/threeTools.js
@@ -23,8 +23,8 @@ function threeTools() {
         this.scene = new THREE.Scene();
     }
     this.setBackdrop = function () {
-        scene.background = new THREE.Color(0xe0e0e0);
-        scene.fog = new THREE.Fog(0xe0e0e0, 20, 100);
+        this.scene.background = new THREE.Color(0xe0e0e0);
+        this.scene.fog = new THREE.Fog(0xe0e0e0, 20, 100);
     }
     this.setBasicCamera = function () {
 
